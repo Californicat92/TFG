1be3516cd153f7ac7816ba76940aff06bb2a2949
christopher juve sancho <1422047@campus.euss.org>
2019-10-26 19:54:37 +0200

    creado documento txt para el informe

--- src/informe/informe.c
+++ src/informe/informe.c
@@ -1,30 +1,40 @@
+//gcc informe.c -o informe -lsqlite3
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h> 
+#include <string.h> 
+
+
+sqlite3 *db;
+char *zErrMsg = 0;
+int rc;
+FILE * fp;
 
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
+
+
+
 	//fprintf(stderr, "%s: ", (const char*)data);
    
    for(i = 0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-      
+   fprintf(fp,"\n");
+   fprintf(fp,"%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
    }
-   
+  
    printf("\n");
    return 0;
 }
 
-/*char getValues(char orden, char sensor){
-		sqlite3 *db;
+char getValues(char orden, char sensor){
+
 		return 0;
 }
-*/
+
 
 int main(int argc, char* argv[]) {
-	sqlite3 *db;
-	char *zErrMsg = 0;
-	int rc;
+
 	char *sql;
 	const char* data = "Callback function called";
 
@@ -37,44 +47,60 @@ int main(int argc, char* argv[]) {
 	} else {
       fprintf(stdout, "\n\n\n");
    }
-	
-int orden = 0;
-	for (orden = 1; orden < 6; orden++)
+	/* Open document */
+	fp = fopen("informe.txt", "w"); 
+    if (fp == NULL) 
+    { 
+        printf("Could not open file"); 
+        return 0; 
+    }  
+      
+	int orden = 0, ID=0;
+	for (ID = 1; ID < 3; ID++)
 	{
-		/*Selección de comando SQL*/
-		if (orden == 1){
-			sql =	"SELECT MIN(Date_time_lecture) FROM Lectures_table " \
-					"WHERE 	ID = 1";
-		}
+		for (orden = 1; orden < 7; orden++)
+		{
+			/*Selección de comando SQL*/
+			if (orden == 1){
+				sql =	"SELECT MIN(Date_time_lecture) FROM Lectures_table " \
+						"WHERE 	ID = 1";
+				//sprintf(mensaje, "SELECT MIN(Date_time_lecture) FROM Lectures_table WHERE ID = %d",ID);
+			}
 
-		else if (orden == 2){
-			sql =	"SELECT MAX(Date_time_lecture) FROM Lectures_table " \
-					"WHERE 	ID = 1";
-		}
+			else if (orden == 2){
+				sql =	"SELECT MAX(Date_time_lecture) FROM Lectures_table " \
+						"WHERE 	ID = 1";
+			}
 
-		else if (orden == 3){
-			sql = 	"SELECT MAX(Value) FROM Lectures_table " \
-					"WHERE 	ID = 2";
-		}
+			else if (orden == 3){
+				sql = 	"SELECT MAX(Value) FROM Lectures_table " \
+						"WHERE 	ID = 2";
+			}
 
-		else if (orden == 4){
-			sql =	"SELECT MIN(Value) FROM Lectures_table " \
-					"WHERE 	ID = 2";
-		}
+			else if (orden == 4){
+				sql =	"SELECT MIN(Value) FROM Lectures_table " \
+						"WHERE 	ID = 2";
+			}
 
-		else if (orden == 5){
-			sql =	"SELECT AVG(Value) FROM Lectures_table " \
-					"WHERE 	ID = 2";
-		}
+			else if (orden == 5){
+				sql =	"SELECT AVG(Value) FROM Lectures_table " \
+						"WHERE 	ID = 2";
+			}
 
-		/* Execute SQL statement */
-		rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+			else if (orden == 6){
+				sql =	"SELECT * FROM Alarms_table";
+			}
 
-		if( rc != SQLITE_OK ) {
-		  fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
+			/* Execute SQL statement */
+			rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+			if( rc != SQLITE_OK ) {
+			  fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			  sqlite3_free(zErrMsg);
+			}
 		}
 	}
+   fclose(fp); 
    sqlite3_close(db);
    return 0;
 }
