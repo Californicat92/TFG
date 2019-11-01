e939de53c0df95c6af4d8b0077847bea9f0e0f8d
Christopher <1422047@campus.euss.org>
2019-11-01 21:03:40 +0100

    corregidos errores con licencia GNU de Linux

--- src/informe/informe.c
+++ src/informe/informe.c
@@ -1,52 +1,52 @@
-/*
- * Informe3.c
- * 
+/* Informe3.c
+ *
  * Copyright 2019  <pi@raspberrypi>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
- 
- /*
+
+/*
  * Formato Captura.db
- * 
- * 	· Sensors_table
- * 		- ID
- * 		- Types (Magnitud)
- * 		- Description
- * 
- * 	· Lectures_table
- * 		- Date_time_lecture
- * 		- ID
- * 		- Value
- * 
- * 	· Alarms_table
- * 		- Date_time_alarm
- * 		- Alarm_description
+ *
+ *	· Sensors_table
+ *		- ID
+ *		- Types (Magnitud)
+ *		- Description
+ *
+ *	· Lectures_table
+ *		- Date_time_lecture
+ *		- ID
+ *		- Value
+ *
+ *	· Alarms_table
+ *		- Date_time_alarm
+ *		- Alarm_description
  */
 
 /*gcc Informe3.c -o informe -lsqlite3
-* arm-linux-gnueabihf-gcc informe.c -o informe -lsqlite3
-*
-* scp nom_executable pi@192.168.11.10X:/tmp
-* (on X és el número de dispositiu si es conecta a través de la xarxa LAN i /tmp és el directori destí).
-* pi@192.168.11.20X:/tmp para WIFI
-*
-*/
+ * arm-linux-gnueabihf-gcc informe.c -o informe -lsqlite3
+ *
+ * scp nom_executable pi@192.168.11.10X:/tmp
+ * (on X és el número de dispositiu si es conecta a través de la xarxa
+ * LAN i /tmp és el directori destí).
+ * pi@192.168.11.20X:/tmp para WIFI
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,19 +54,21 @@
 #include <string.h>
 FILE * fp;
 
-static int getValues(void *data, int argc, char **argv, char **azColName){
+static int getValues(void *data, int argc, char **argv, char **azColName)
+{
 	int i;
-	
-	memset((char *)data,'\0',sizeof(data));
-	
-	for (i = 0; i < argc; i++){
+
+	memset((char *)data, '\0', sizeof(data));
+
+	for (i = 0; i < argc; i++) {
 		sprintf(data, "%s", argv[i] ? argv[i] : "NULL");
 		//~ printf("%s", (char *)data);
 	}
 	return 0;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	sqlite3 *db;
 	char sql[120];
 	char *zErrMsg = 0;
@@ -75,120 +77,118 @@ int main(int argc, char* argv[]){
 	int nAlarm;
 	char data[200];
 	char texto[400];
-	char *fecha,*hora;	
+	char *fecha, *hora;
 	/* Open database */
 	rc = sqlite3_open("captura.db", &db);
 
-	if( rc ) {
+	if (rc)
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return(0);
-	}
 
 	/* Open document */
-	fp = fopen("informe.txt", "w"); 
-    if (fp == NULL){ 
-        printf("Could not open file"); 
-        return 0; 
-    }
-    
-    /* Lectura nº sensores */
-	memset(data,'\0',sizeof(data));
-	sprintf(sql, "SELECT MAX(ID) FROM Sensors_table");
-	
-	/* Execute SQL statement */
-	rc = sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
+	fp = fopen("informe.txt", "w");
+	if (fp == NULL)
+		printf("Could not open file");
 
-	if( rc != SQLITE_OK ) {
-	  fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	  sqlite3_free(zErrMsg);
+    /* Lectura nº sensores */
+	memset(data, '\0', sizeof(data));
+	sprintf(sql, "SELECT MAX(ID) FROM Sensors_table");
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 	}
 	nSen = atoi(data);
 	//~ printf("\n\nEl numero de sensores es %d\n",nSen);
 	int n;
-	for (n = 1; n <= nSen; n++){
+
+	for (n = 1; n <= nSen; n++) {
 		//Sensor
 		sprintf(texto, "\nInforme de sensor %d:\n", n);
-		fprintf(fp,"%s", texto);
-		memset(texto,'\0', sizeof(texto));
-		
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+
 		//Fecha/hora inicial
 		sprintf(sql, "SELECT MIN(Date_time_lecture) FROM Lectures_table WHERE ID = %d", n);
-		rc = sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
-		fecha = strtok(data," ");
-		hora = strtok(hora," ");
-		sprintf(texto, "\tLa lectura empieza el dia: %s a las: %s\n", fecha,hora);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
-		
+		rc = sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+		fecha = strtok(data, " ");
+		hora = strtok(hora, " ");
+		sprintf(texto, "\tLa lectura empieza el dia: %s a las: %s\n", fecha, hora);
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+
 		//Fecha/hora final
 		sprintf(sql, "SELECT MAX(Date_time_lecture) FROM Lectures_table WHERE ID = %d", n);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
-		fecha = strtok(data," ");
-		hora = strtok(hora," ");
-		sprintf(texto, "\tLa lectura acaba el dia: %s a las: %s\n", fecha,hora);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
-		
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+		fecha = strtok(data, " ");
+		hora = strtok(hora, " ");
+		sprintf(texto, "\tLa lectura acaba el dia: %s a las: %s\n", fecha, hora);
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+
 		//Valor máximo
 		sprintf(sql, "SELECT MAX(Value) FROM Lectures_table WHERE ID = %d", n);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
 		sprintf(texto, "\tEl valor máximo de tensión registrado: %s V\n", data);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
-		
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+
 		//Valor mínimo
 		sprintf(sql, "SELECT MIN(Value) FROM Lectures_table WHERE ID = %d", n);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
 		sprintf(texto, "\tEl valor mínimo de tensión registrado: %s V\n", data);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
-		
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+
 		//Valor medio
 		sprintf(sql, "SELECT AVG(Value) FROM Lectures_table WHERE ID = %d", n);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
 		sprintf(texto, "\tEl valor medio de tensión obtenido: %s V\n\n", data);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
 	}
-	if( rc != SQLITE_OK ) {
-	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	sqlite3_free(zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 	}
-	
+
 	// Obtener numero de alarmas de la BBDD en SQL
 	sprintf(sql, "SELECT Count(*) FROM Alarms_table");
 	/* Execute SQL statement */
-	rc = sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
-	if( rc != SQLITE_OK ) {
-	  fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	  sqlite3_free(zErrMsg);
+	rc = sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 	}
 	nAlarm = atoi(data);
 	// Imprimir alarmas
 	int x;
+
 	fprintf(fp, "\n\nListado de alarmas:\n\n");
-	for (x = 1; x <= nAlarm; x++){
-		sprintf(sql, "SELECT Date_time_alarm FROM Alarms_table WHERE rowid = %d",x);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
-		fecha = strtok(data," ");
-		hora = strtok(hora," ");
-		sprintf(texto, "\t%d.- Alarma ocurrida el dia: %s a las: %s\n", x,fecha,hora);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
-		sprintf(sql, "SELECT Alarm_description FROM Alarms_table WHERE rowid = %d",x);
-		sqlite3_exec(db, sql, getValues, (void*)data, &zErrMsg);
-		sprintf(texto, "\tDescripción de la alarma: %s\n\n",data);
-		fprintf(fp,"%s",texto);
-		memset(texto,'\0', sizeof(texto));
+	for (x = 1; x <= nAlarm; x++) {
+		sprintf(sql, "SELECT Date_time_alarm FROM Alarms_table WHERE rowid = %d", x);
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+		fecha = strtok(data, " ");
+		hora = strtok(hora, " ");
+		sprintf(texto, "\t%d.- Alarma ocurrida el dia: %s a las: %s\n", x, fecha, hora);
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
+		sprintf(sql, "SELECT Alarm_description FROM Alarms_table WHERE rowid = %d", x);
+		sqlite3_exec(db, sql, getValues, (void *)data, &zErrMsg);
+		sprintf(texto, "\tDescripción de la alarma: %s\n\n", data);
+		fprintf(fp, "%s", texto);
+		memset(texto, '\0', sizeof(texto));
 	}
 
-	if( rc != SQLITE_OK ) {
-	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	sqlite3_free(zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 	} else {
-	printf("\nEjecución correcta, revise el documento informe.txt\n\n");
+		printf("\nEjecución correcta, revise el documento informe.txt\n\n");
 	}
-		
+
 	fclose(fp);
 	sqlite3_close(db);
 	return 0;
