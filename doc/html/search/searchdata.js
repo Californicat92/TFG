12827488f28b53586f2b9916d5ceb88258cc23d6
Christopher <1422047@campus.euss.org>
2019-11-03 11:49:26 +0100

    movido Doxygen a la raiz para compartirlo entre Captura / Informe

---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "afgim",
+  1: "a",
+  2: "i",
+  3: "gm",
+  4: "f"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "files",
+  3: "functions",
+  4: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Namespaces",
+  2: "Archivos",
+  3: "Funciones",
+  4: "Variables"
+};
+
